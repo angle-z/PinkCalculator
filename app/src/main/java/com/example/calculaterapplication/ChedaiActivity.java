c1c7ceb4e9fdcfbffa199d9989c4a8c5849592ba
angle-z <1027093960@qq.com>
2021-10-16 09:46:12 +0800

    Edit: ChedaiActivity.java

--- app/src/main/java/com/example/calculaterapplication/ChedaiActivity.java
+++ app/src/main/java/com/example/calculaterapplication/ChedaiActivity.java
@@ -3,7 +3,7 @@ package com.example.calculaterapplication;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-
+// 车贷计算
 public class ChedaiActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
